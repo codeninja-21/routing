3b3bd98512e80deedb0b8fd4504f2d56827191ed
afif.dev <salah7server@gmail.com>
2025-12-15 09:41:55 +0700

    lupa add 😭

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav style={{ marginBottom: 20 }}>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link> |{" "}
+        <Link to="/products">Products</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
